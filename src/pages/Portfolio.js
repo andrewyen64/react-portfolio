c845a961f281f1c2518607f3c33f4d6887b0a97d
Andrew <andrewyen64@gmail.com>
2020-10-05 22:00:25 -0700

    Add Portfolio page (forgot to last commit)

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+import Cards from "../components/ProjectCards/ProjectCards.js";
+
+function Portfolio() {
+  return (
+    <div>
+      <Cards />
+    </div>
+  );
+}
+
+export default Portfolio;
