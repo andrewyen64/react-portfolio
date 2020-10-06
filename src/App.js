1ad50afe20bab47eb9be3e50eb51a196d6d5de5a
Andrew <andrewyen64@gmail.com>
2020-10-05 21:55:52 -0700

    Add initial files and updated App.js/index.html

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav.js";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/react-portfolio" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
