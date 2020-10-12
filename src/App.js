e40bc3bf917f1b0c47f291796c320890299286a0
Andrew <andrewyen64@gmail.com>
2020-10-11 22:55:06 -0700

    Fix routing issues

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
       <div className="App">
         <Nav />
         <Switch>
-          <Route exact path="/" component={About} />
+          {/* <Route exact path="/" component={About} /> */}
           <Route exact path="/react-portfolio" component={About} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/react-portfolio/portfolio" component={Portfolio} />
+          <Route exact path="/react-portfolio/contact" component={Contact} />
         </Switch>
         <Footer />
       </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,19 +6,19 @@ function Nav() {
   return (
     <div>
       <nav className="navbar bg-dark navbar-expand-sm">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/react-portfolio">
           <span class="brandName">Andrew Yen</span>
         </Link>
         <div>
           <ul className="navbar-nav ml-auto">
             <Link className="nav-link border-light active hover" 
-              to="/">
+              to="/react-portfolio">
               <li>About</li>
             </Link>
-            <Link className="nav-link active hover" to="/portfolio">
+            <Link className="nav-link active hover" to="/react-portfolio/portfolio">
               <li>Portfolio</li>
             </Link>
-            <Link className="nav-link active hover" to="/contact">
+            <Link className="nav-link active hover" to="/react-portfolio/contact">
               <li>Contact</li>
             </Link>
           </ul>
