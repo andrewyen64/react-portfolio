d7a9ebe8145159e98996f7cd980a96fa4779fbe2
Andrew <andrewyen64@gmail.com>
2020-10-11 22:48:59 -0700

    Update bio description and add tech skills, remove resume link

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Bio.css";
 import profilePic from "../../images/profilepic.jpg"
 
@@ -12,28 +11,29 @@ function Bio () {
       <img id = "about-image"
         src = {profilePic}
         alt = "My Profile"
-        width = "230px" height = "300px" 
+        width = "300px" height = "390px" 
         />
       <p>
           Hello there! My name is Andrew Yen. I'm a Full Stack web developer educated at UC Riverside Extension where I earned 
-          a Certificate in Full-Stack Web Development and developed skills in the Front and Backend.
+          a Certificate in Full-Stack Web Development.
           <br/><br/>
           My huge passion for a variety of video games (platformers, fighters, action and tactical RPGs) has given me some 
-          knowledge on UIs and an enjoyable experience.  
+          knowledge on UIs and an enjoyable user experience. It's also what motivated me to start leaning how to code so that I may also  
+          contribute to the gaming/tech industry that's brought me so much joy and memories.
           <br/><br/>
-          As a video game enthusiast who has seen all sorts of UIs (the good and the bad), I aim to create clean projects that 
+          As a video game enthusiast who has utilized all sorts of UIs (the good and the bad), I aim to create clean projects that 
           deliver a fulfilling, positive experience to the user.
           <br/><br/>
+          My current tech skills include:
+          <ul>
+            <li>Front-end: HTML, CSS, JS, jQuery, Bootstrap, Materialize CSS, REST APIs, Server-side APIs, AJAX, PWAs, React.js </li>
+            <li>Back-end: Node.js, Express, Axios, MySQL, Sequelize, MongoDB, Mongoose </li>
+          </ul>
           I'm excited to work with others to create great projects and learn new skills. 
           <br></br>
-          Feel free to send me a message
-          to chat about possible future projects <Link to="/contact">here</Link>!
+          <br></br>
+          Feel free to send me a message via my contact page info!
           <br/><br/>
-          Link to my resume:
-          <a href="https://docs.google.com/document/d/e/2PACX-1vSty7EOEJ1yaO4bQOlkTizTDC_tLJlu25hKSr87LQ_3e5iB4OyRJgCTuvwnX9mVoOz_H6mfzkqb1CEq/pub" 
-          target="_blank" rel="noopener noreferrer" >
-          Yen_Andrew_2020
-          </a>
 
       </p>
 
